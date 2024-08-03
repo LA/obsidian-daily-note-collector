b9472eedd3062cd35a64d77881541e9726898e29
Adar <14169758+LA@users.noreply.github.com>
2024-08-03 15:03:51 -0700

    implement daily note collector

--- main.ts
+++ main.ts
@@ -1,134 +1,95 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin, TAbstractFile, TFile } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class DailyNoteCollectorPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		this.registerEvent(
+			this.app.vault.on("create", this.onCreateFile.bind(this))
+		);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(
+			this.app.vault.on("rename", this.onRenameFile.bind(this))
+		);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.registerEvent(
+			this.app.vault.on("delete", this.onDeleteFile.bind(this))
+		);
+	}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+	onunload() {}
+
+	onCreateFile(file: TAbstractFile) {
+		if (!(file instanceof TFile)) return;
+		const link = `[[${this.removeExtension(file.path)}]]`;
+		const { dailyNote } = this.getDailyNote();
+		if (file.path === dailyNote) {
+			return;
+		}
+		const dailyNoteFile = this.app.vault.getFileByPath(dailyNote);
+		if (!dailyNoteFile) {
+			this.app.vault.create(dailyNote, `- ${link}`);
+		} else {
+			this.app.vault
+				.read(dailyNoteFile)
+				.then((content) => {
+					if (content.includes(link)) {
+						return;
 					}
+					const newContent = content + `\n- ${link}`;
+					this.app.vault.modify(dailyNoteFile, newContent);
+				})
+				.catch(() => {
+					new Notice("Error updating daily note");
+				});
+		}
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+	onDeleteFile(file: TAbstractFile) {
+		if (!(file instanceof TFile)) return;
+		const { dailyNote } = this.getDailyNote();
+		const dailyNoteFile = this.app.vault.getFileByPath(dailyNote);
+		if (!dailyNoteFile) {
+			return;
+		}
+		this.app.vault.read(dailyNoteFile).then((content) => {
+			const newContent = content.replace(
+				`- [[${this.removeExtension(file.path)}]]`,
+				""
+			);
+			this.app.vault.modify(dailyNoteFile, newContent);
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
+	onRenameFile(file: TAbstractFile, oldPath: string) {
+		/** Rename support is not necessary if you let Obsidian update existing links */
+		// if (!(file instanceof TFile)) return;
+		// const { dailyNote } = this.getDailyNote();
+		// const dailyNoteFile = this.app.vault.getFileByPath(dailyNote);
+		// if (!dailyNoteFile) {
+		// 	return;
+		// }
+		// this.app.vault.read(dailyNoteFile).then((content) => {
+		// 	const newContent = content.replace(
+		// 		`[[${this.removeExtension(oldPath)}]]`,
+		// 		`[[${this.removeExtension(file.path)}]]`
+		// 	);
+		// 	this.app.vault.modify(dailyNoteFile, newContent);
+		// });
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	removeExtension(path: string) {
+		const split = path.split(".");
+		return split.slice(0, split.length - 1).join("");
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	getDailyNote() {
+		const date = new Date();
+		const year = date.getFullYear();
+		const month = (date.getMonth() + 1).toString().padStart(2, "0");
+		const day = date.getDate().toString().padStart(2, "0");
+		const dateKey = `${year}-${month}-${day}`;
+		const dailyNote = `${dateKey}.md`;
+		return {
+			dailyNote,
+		};
 	}
 }
